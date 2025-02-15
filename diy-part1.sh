577458d2eee391e157182f17f55cb3cc1ee4b77b
Pururut-iris <30848574+Pururut-iris@users.noreply.github.com>
2025-02-15 22:14:26 +0800

    Update diy-part1.sh
    by ai

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,19 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+#
+#
+#
+#!/bin/bash
+# =========================================================
+# 添加软件包源（在更新 feeds 前执行）
+# =========================================================
+
+# 1. 添加 iStore 插件源
+#echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+# 2. 添加 AdGuard Home 核心源（确保最新版本）
+#echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome' >> feeds.conf.default
+
+# 3. 添加 Argon 主题源
+#echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >> feeds.conf.default
